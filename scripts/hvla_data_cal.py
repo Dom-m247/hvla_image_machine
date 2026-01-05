7c08a8978b47694f45828ee590284fbb5d9288fd
Dominic M <dominic.m209.247@gmail.com>
2026-01-05 18:04:40 -0500

    added better support for importing existing .ms

--- scripts/hvla_data_cal.py
+++ scripts/hvla_data_cal.py
@@ -2,7 +2,8 @@ FULLMS = 'fullSet' #+".ms"
 import casatasks as ct
 import sys,os
 from .data_class import data
-from .data_calibration import parse_list_obs as parse
+#from .data_calibration import parse_list_obs as parse
+from .data_calibration import *
 import pprint as pp
 
 
@@ -11,10 +12,10 @@ def convert_to_ms(archive):
   Converts raw HVLA data archive to Measurement Set (MS) format
   """
   #OUTPUT MS name = "fullMS.ms" -> weird cstring error if not directly entered.
-  FULLMS = 'fullSet'
   if (archive.endswith('.ms')):
     print(f"Archive {archive} is already in MS format.")
-    return
+    archive = archive[:len(archive)-3]
+    return parse.log_listobs(archive)
   #import archive to MS
   print(f"Converting archive {archive} to Measurement Set format...")
   try:
@@ -23,7 +24,7 @@ def convert_to_ms(archive):
     return parse.log_listobs(FULLMS)
 
   except RuntimeError as file_exists:
-    print(f"Vis file already exists, delete it and re-run")
+    print(f"MS file already exists, delete it and re-run")
     sys.exit() # add call to a cleanup script?
     
 
@@ -33,7 +34,13 @@ def data_cal(options):
   Creates MS files from raw data, applies calibration
   """
   list_obs = convert_to_ms(options.get_dict()['archive_file']) 
+  
+  #extract info from listobs -> more complete than returned data
   parse.parseListObs(list_obs,options) 
+
+  #find calibrators TODO: add phase call!
+  find_cal.find_amp_cal(options)
+
 
 
 
