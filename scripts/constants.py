059d1703bafbcc5f86f0141f1a1b2730afd8144b
Dominic M <dominic.m209.247@gmail.com>
2026-01-05 18:03:29 -0500

    included for the occasinal constant IE. Amp cals

---
+++ scripts/constants.py
@@ -0,0 +1,21 @@
+#Constants that may be integrated, but are being placed here for reference!
+BAND_GHZ_RANGES = {'L': [0.985, 2.025], 
+                   'S': [2.026, 4.013], 
+                   'C': [4.309, 7.461], 
+                   'X': [8.000, 11.512], 
+                   'Ku': [13.133, 18.000], 
+                   'K': [20.000, 26.412], 
+                   'Ka': [28.258, 37.011], 
+                   'Q': [43.148, 48.873]}
+
+
+COMMON_AMPCALS_DICT = {'1331+305': '3C286', 
+                      '1328+307': '3C286', 
+                      '0542+4951': '3C147', 
+                      '0137+3309': '3C48', 
+                      '0134+329': '3C48', 
+                      '0137+331': '3C48', 
+                      '0521+1638': '3C138'}
+
+#Measurment set Names
+FULLMS = 'fullSet' # for the full MS before splitting
