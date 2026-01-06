a5215d2e327d5a3505c235e719cb2279259a045d
Dominic M <dominic.m209.247@gmail.com>
2026-01-06 16:34:59 -0500

    Added mhz band values, directory names, and export keys

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,3 +1,10 @@
+#parent directory name, edit if different!
+FOLDER_NAME = "hvla_script_proj"
+
+#Measurment set Names
+FULLMS = 'fullSet' # for the full MS before splitting
+AMP_CAL_MS = 'init' #'amp_cal_set'
+
 #Constants that may be integrated, but are being placed here for reference!
 BAND_GHZ_RANGES = {'L': [0.985, 2.025], 
                    'S': [2.026, 4.013], 
@@ -7,6 +14,14 @@ BAND_GHZ_RANGES = {'L': [0.985, 2.025],
                    'K': [20.000, 26.412], 
                    'Ka': [28.258, 37.011], 
                    'Q': [43.148, 48.873]}
+BAND_MHZ_RANGES = {"L": [985.0, 2025.0], 
+                   "S": [2026.0, 4013.0], 
+                   "C": [4309.0, 7461.0], 
+                   "X": [8000.0, 11512.0], 
+                   "Ku": [13133.0, 18000.0], 
+                   "K": [20000.0, 26412.0], 
+                   "Ka": [28258.0, 37011.0], 
+                   "Q": [43148.0, 48873.0],}
 
 
 COMMON_AMPCALS_DICT = {'1331+305': '3C286', 
@@ -17,5 +32,9 @@ COMMON_AMPCALS_DICT = {'1331+305': '3C286',
                       '0137+331': '3C48', 
                       '0521+1638': '3C138'}
 
-#Measurment set Names
-FULLMS = 'fullSet' # for the full MS before splitting
+EXPORT_KEYS = ["source","archive_file", "band", "breakpoints", 
+               "solint","custom_amp_cal", "reference_antenna", 
+               "amp_cal_source","model"] #amp_cal_source + model import not supported (over written)
+
+
+
